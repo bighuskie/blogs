a784451b02f08c17e77a87cc28cd90c4f7f6521f
B.Huskie <1665370368@qq.com>
2019-05-02 09:21:53 +0800

    增加路由规则

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,6 +2,8 @@ import React from "react";
 //可以向组件注入store数据
 import { Provider } from "react-redux";
 import store from "../../store/store";
+//引入路由
+import { BrowserRouter, Route } from "react-router-dom";
 import "./style.scss";
 import Header from "../header";
 
@@ -11,6 +13,10 @@ class App extends React.Component {
       <div>
         <Provider store={store}>
           <Header />
+          <BrowserRouter>
+            <Route path="/" exact render={() => <div>hello</div>} />
+            <Route path="/index" exact render={() => <div>world</div>} />
+          </BrowserRouter>
         </Provider>
       </div>
     );
